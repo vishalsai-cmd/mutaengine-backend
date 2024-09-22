577ae0ad7bfc9411fb2f0ed555ccdbbb1c4ad66d
vishalsai-cmd <129299772+vishalsai-cmd@users.noreply.github.com>
2024-09-22 14:23:34 +0530

    Update login_component.js

--- client/src/components/login_component.js
+++ client/src/components/login_component.js
@@ -23,7 +23,7 @@ export default function Login() {
       return;
     }
 
-    fetch("http://localhost:5000/login-user", {
+    fetch("mutaengine-backend-zhy5.vercel.app/login-user", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
